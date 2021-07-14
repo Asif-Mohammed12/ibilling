b45fb29981b611152f43cadf7c36914ce9d765cc
acheapasif <73394164+acheapasif@users.noreply.github.com>
2021-07-14 13:38:07 +0530

    config update

--- application/config.php
+++ application/config.php
@@ -1,7 +1,7 @@
 <?php
-$db_host	    = 'localhost';
-$db_user        = 'root';
+$db_host	    = 'sql6.freemysqlhosting.net';
+$db_user        = 'sql6425097';
 $db_password	= '';
-$db_name	    = 'billing';
-define('APP_URL', 'http://127.0.0.1:8080/billing/ibilling');
+$db_name	    = 'sql6425097';
+define('APP_URL', 'https://eightteenplusbilling.herokuapp.com/');
 $_app_stage = 'Live'; // You can set this variable Live to Dev to enable ibilling Debug
