c5c3680a2943b0b9220b9dcff0bfec92ecb231c9
acheapasif <73394164+acheapasif@users.noreply.github.com>
2021-07-14 12:51:07 +0530

    first commit

---
+++ application/config.php
@@ -0,0 +1,7 @@
+<?php
+$db_host	    = 'localhost';
+$db_user        = 'root';
+$db_password	= '';
+$db_name	    = 'billing';
+define('APP_URL', 'http://127.0.0.1:8080/billing/ibilling');
+$_app_stage = 'Live'; // You can set this variable Live to Dev to enable ibilling Debug
